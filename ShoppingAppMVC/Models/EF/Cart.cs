a42f5a5bfaa753e64c29307836561bf1365a8f70
Jacob Wolfe <jacob297@revature.net>
2022-09-06 14:40:52 -0500

    First Commit of Skeleton Code provided by Alec.

---
+++ ShoppingAppMVC/Models/EF/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingAppMVC.Models.EF
+{
+    public partial class Cart
+    {
+        public string? ItemName { get; set; }
+        public decimal? Cost { get; set; }
+
+        public virtual Item? ItemNameNavigation { get; set; }
+    }
+}
---
+++ ShoppingAppMVC/Models/EF/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingAppMVC.Models.EF
+{
+    public partial class User
+    {
+        public string? Username { get; set; }
+        public string Password { get; set; } = null!;
+        public int UserNo { get; set; }
+    }
+}
