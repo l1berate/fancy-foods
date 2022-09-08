2c8a2002c36475955e657e1a9dbcc942490d1b73
Jacob Wolfe <jacob297@revature.net>
2022-09-07 23:06:25 -0500

    Removed the code in shoppingDBContext.cs file that explicitly showed the SQLServer connection string. Program.cs already pulls in the connection string from appsettings.json

--- ShoppingAppMVC/Models/EF/shoppingDBContext.cs
+++ ShoppingAppMVC/Models/EF/shoppingDBContext.cs
@@ -20,17 +20,6 @@ namespace ShoppingAppMVC.Models.EF
         public virtual DbSet<Item> Items { get; set; } = null!;
         public virtual DbSet<User> Users { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#pragma warning disable CS1030 // #warning directive
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=tcp:nikhilshah-project-server.database.windows.net, 1433;Initial Catalog=shoppingDB;Persist Security Info=False;User ID=project;Password=Cohort@1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
-#pragma warning restore CS1030 // #warning directive
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Cart>(entity =>
