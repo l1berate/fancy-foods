a0a44d7b80929359b5cde0c829daf35d4cc51590
Alec Battest <AlecBattest@gmail.com>
2022-09-10 12:13:30 -0500

    -Modified User table in DB to list UserNo first since it's the primary key -Switched the order of properties in User.cs (don't know if it mattered)

--- ShoppingAppMVC/Models/EF/User.cs
+++ ShoppingAppMVC/Models/EF/User.cs
@@ -5,8 +5,8 @@ namespace ShoppingAppMVC.Models.EF
 {
     public partial class User
     {
+        public int UserNo { get; set; }
         public string? Username { get; set; }
         public string Password { get; set; } = null!;
-        public int UserNo { get; set; }
     }
 }
