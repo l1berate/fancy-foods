15f72afaff8f478180820ae7fb396f641397df9f
Alec Battest <AlecBattest@gmail.com>
2022-09-09 20:12:16 -0500

    Added UpdateCart and RemoveFromCart -Remove Works, Update doesn't

--- ShoppingAppMVC/Controllers/CartController.cs
+++ ShoppingAppMVC/Controllers/CartController.cs
@@ -30,5 +30,47 @@ namespace ShoppingAppMVC.Controllers
 
             return RedirectToAction("Index", "Cart");
         }
+
+
+        public async Task<IActionResult> UpdateCart(int id, int quantity)
+        {
+            if (_context.Carts == null)
+            {
+                return Problem("Entity set 'shoppingDBContext.Cart' is null");
+            }
+
+            var product = await _context.Carts.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+        //Received an error that the element couldn't be translated. But I think this is the right way to do it
+            _context.Carts.ElementAt(id).Quantity = quantity;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Cart");
+        }
+
+
+    //Working url to remove items from cart based on id ~Cart/RemoveFromCart?id=1
+    //Can remove by ID in the Cart Table
+    //Cannot reset ID back to 1
+        public async Task<IActionResult> RemoveFromCart(int id)
+        {
+            if (_context.Carts == null)
+            {
+                return Problem("Entity set 'shoppingDBContext.Cart' is null");
+            }
+
+            var product = await _context.Carts.FindAsync(id);
+            if (product != null)
+            {
+                _context.Carts.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Cart");
+        }
     }
 }
