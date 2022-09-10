be763da69767087d3e2a9e20e1e4150b81a33e8a
Alec Battest <AlecBattest@gmail.com>
2022-09-10 14:00:09 -0500

    Created shopping controller that requires login to checkout

---
+++ ShoppingAppMVC/Controllers/ShoppingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingAppMVC.Controllers
+{
+    [Authorize]
+    public class ShoppingController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
